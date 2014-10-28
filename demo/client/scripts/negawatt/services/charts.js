ca2b2f673f941026d948f3794eb3f7e4bf592c5d
Carlos Mantilla <cmantilla@aliongroo.com>
2014-10-28 17:20:50 +0200

    WIP: refactor chart plotting.

--- demo/client/scripts/negawatt/services/charts.js
+++ demo/client/scripts/negawatt/services/charts.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('app')
-  .service('Chart', function () {
+  .service('Chart', function ($q) {
+    var Chart = this;
     var colors = {
       primary: '#5B90BF',
       success: '#A3BE8C',
@@ -105,5 +106,180 @@ angular.module('app')
 
       return angular.extend(options, basicOptions);
     };
+
+
+    /**
+     * Return object expected by the pieChart component.
+     *
+     * @returns {*} - data object.
+     */
+    this.mockPieChart = function() {
+      
+      // Define basic configuration.
+      var mock = {
+        data: [],
+        options: {
+          series: {
+            pie: {
+              show: true
+            }
+          },
+          legend: {
+            show: true
+          },
+          grid: {
+            hoverable: true,
+            clickable: true
+          },
+          colors: [
+            colors.primary,
+            colors.success,
+            colors.info,
+            colors.warning,
+            colors.danger
+          ],
+          tooltip: true,
+          tooltipOpts: {
+            content: "%p.0%, %s",
+            defaultTheme: false
+          }
+        }
+      };
+
+      // Set demo data
+      mock.data = [
+        {
+          label: "תאורת חוץ",
+          data: 3126998
+        }, {
+          label: "חינוך",
+          data: 2852161
+        }, {
+          label: "עיריה",
+          data: 646544
+        }, {
+          label: "רווחה",
+          data: 312385
+        }, {
+          label: "שרותי דת",
+          data: 161725
+        }, {
+          label: "בריאות",
+          data: 79081
+        }, {
+          label: "בטחון",
+          data: 71611
+        }, {
+          label: "ספורט",
+          data: 46440
+        }, {
+          label: "עמותה",
+          data: 26378
+        }
+      ];
+
+      //
+      mock.options = {
+        series: {
+          pie: {
+            show: true
+          }
+        },
+        legend: {
+          show: true
+        },
+        grid: {
+          hoverable: true,
+          clickable: true
+        },
+        colors: [
+          colors.primary,
+          colors.success,
+          colors.info,
+          colors.warning,
+          colors.danger
+        ],
+        tooltip: true,
+        tooltipOpts: {
+          content: "%p.0%, %s",
+          defaultTheme: false
+        }
+      };
+            
+      return mock;
+    }
+
+    /**
+     * Get the response data from the service electricity via controller report.
+     *
+     * @param {*} - response
+     * @returns {$q.promise}
+     */
+    this.getLineChart = function(response) {
+      var chart,
+        deferred = $q.defer();
+
+      function chartData() {
+        return [
+          [1, response.data.data.kwh]
+        ]
+      }
+
+      // Define basic label confifuration.
+      chart = {
+        line1: {
+          options: Chart.options('lastYear'),
+          data: [
+            {
+              label: 'תעריף שיא',
+              data: chartData
+            }
+          ]
+        }
+      };
+
+      //// Update x axis labels.
+      //$scope.line1;
+      //
+      ////Update y axis data.
+      chart.line1.data[0].data= [
+        [1,125],
+        [2,345],
+        [3,245],
+        [4,658],
+        [5,659],
+        [6,125],
+        [7,345],
+        [8,245],
+        [9,658],
+        [10,659],
+        [11,659],
+        [12,659]
+      ];
+
+      //
+      //data: [
+      //  {
+      //    label: 'תעריף שיא'
+      //  },
+      //  {
+      //    label: 'תעריף גבע'
+      //  },
+      //  {
+      //    label: 'תעריף שפל'
+      //  },
+      //  {
+      //    label: 'ללא תעו״ז'
+      //  }
+      //]
+      //
+      //// Update detailed.
+      //$scope.line1.data[1].data = response.data.medium;
+      //$scope.line1.data[2].data = response.data.low;
+      //$scope.line1.data[3].data = response.data.fixed;
+
+      deferred.resolve(chart);
+      return deferred.promise;
+    }
 
   });
