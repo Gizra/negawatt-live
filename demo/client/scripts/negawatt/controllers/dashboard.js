e07d8a2caee61e575cc8082a768f162602c411ab
Carlos Mantilla <cmantilla@aliongroo.com>
2014-11-02 11:23:52 +0200

    feat: activate click listener on the leaflet map.

--- demo/client/scripts/negawatt/controllers/dashboard.js
+++ demo/client/scripts/negawatt/controllers/dashboard.js
@@ -1,31 +1,34 @@
 'use strict';
 
 /**
-* Dashboard controller.
-*/
+ * Dashboard controller.
+ */
 angular.module('app')
+  .controller('DashboardCtrl', function ($scope, Meter) {
+    $scope.city = {
+      'name': 'קרית גת',
+      'lat': 31.603466403994243,
+      'lng': 34.77099895477295,
+      'zoom': 14
+    };
 
-.controller('DashboardCtrl', function($scope, Meter) {
-  $scope.city = {
-  'name': 'קרית גת',
-  'lat': 31.603466403994243,
-  'lng': 34.77099895477295,
-  'zoom': 14
-};
+    $scope.events = {
+      markers: {
+        enable: ['click'],
+        logic: 'emit'
+      }
+    };
 
-$scope.events = {
-  'name': 'קרית גת',
-  'lat': 31.603466403994243,
-  'lng': 34.77099895477295,
-  'zoom': 14
-};
+    // Get list of meters.
+    Meter.get().then(function (response) {
+      $scope.city = response.data.city;
+      $scope.meters = response.data.meters;
+      $scope.status = response.data.status;
+      $scope.totals = response.data.totals;
+    });
 
-// Get list of meters.
-  Meter.get().then(function(response) {
-    $scope.city = response.data.city;
-  $scope.meters = response.data.meters;
-  $scope.status = response.data.status;
-  $scope.totals = response.data.totals;
+    $scope.$on('leafletDirectiveMap.click', function(event){
+      console.log('event:', event);
+    });
+
   });
-
-});
